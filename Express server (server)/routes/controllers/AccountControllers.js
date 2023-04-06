aed134b3b346b368cd43537afae6e4f16f48d358
braxtonwright <33258249+braxtonwright@users.noreply.github.com>
2023-04-05 20:39:55 -0600

    Fixed issues with the AccountContoller.js file

--- Express server (server)/routes/controllers/AccountControllers.js
+++ Express server (server)/routes/controllers/AccountControllers.js
@@ -1,20 +1,12 @@
 const {
-<<<<<<<< HEAD:Express server (server)/routes/controllers/AccountControllers.js
     AppPlayerLoginInfo,
-========
-    AppGameLogic
->>>>>>>> 5acd124451fa26c67d4bd16aec4c52948d3f91f0:Express server (server)/routes/controllers/GameLogic.js
 } = require("../../models/models");  //include our "models.js" module so we can use it inside this file.  Module documentation https://www.w3schools.com/nodejs/nodejs_modules.asp
 
 //make your routes here
 exports.ControllerToRead = (req, res) => {
     //inside the {} you can filter results that are returned by using the syntax "fieldName: value" with a comma between each condition
     //This can be further filtered by after the {}, you can add the following code ", 'FieldName1 FieldName2 ect.'".  This will only return the fields you specified inside the ''.
-<<<<<<<< HEAD:Express server (server)/routes/controllers/AccountControllers.js
     AppPlayerLoginInfo.find({})
-========
-    AppGameLogic.find({})
->>>>>>>> 5acd124451fa26c67d4bd16aec4c52948d3f91f0:Express server (server)/routes/controllers/GameLogic.js
         .then((returnedData) => {
             console.log({ returnedData });
             res.json(returnedData);
@@ -27,7 +19,7 @@ exports.ControllerToRead = (req, res) => {
             });
         });
 };
-{/* 
+
 exports.ControllerToCreate = (req, res) => {
     AppPlayerLoginInfo.create(req.body)  //req.body contains all the data that will be used to create a new entry for the DB
         .then((createdData) => {
@@ -80,4 +72,4 @@ exports.ControllerToDelete = (req, res) => {
             error: err.message,
             });
         });
-}; */}
+};
