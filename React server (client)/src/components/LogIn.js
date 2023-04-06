005b02cd85c4ab61bb7a3a8bb0d1a1c2ea15199e
Zack Otterstrom <90364473+zackaryO@users.noreply.github.com>
2023-04-05 20:42:11 -0600

    remove imports

--- React server (client)/src/components/LogIn.js
+++ React server (client)/src/components/LogIn.js
@@ -1,10 +1,9 @@
-import { useState, useEffect } from 'react';
+
 import axios from 'axios';
 
-import { useNavigate } from 'react-router-dom';
-import sha256 from 'js-sha256';
+// import sha256 from 'js-sha256';
 
-function LoginForm(props) {
+function LoginForm() {
   
 
     return (
