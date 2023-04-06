efd6467205e11f43140e05a85e6798b87408a3c9
Zack Otterstrom <90364473+zackaryO@users.noreply.github.com>
2023-04-05 20:53:34 -0600

    a

--- React server (client)/src/components/LogIn.js
+++ React server (client)/src/components/LogIn.js
@@ -9,24 +9,24 @@ function LoginForm() {
     return (
 <div>
       <section>
-        <h1>Welcome to Hangman!</h1>
+        <h1>Welcome to Grid Word Finder!</h1>
         <h2>Login or Create a New Account</h2>
         <br />
         <form>
           <label>
             Username:
-            <input type="text" value={UserName} />
+            <input type="text"  />
           </label>
           <br />
           <label>
             Password:
-            <input type="password" value={password}  />
+            <input type="password"   />
           </label>
           <br />
           <br />
           <button type="submit">Login</button>
           <br />          <br />
-          <button onClick={handleClick}>Create New Account</button>
+          <button >Create New Account</button>
         </form>
       </section>
     </div>
